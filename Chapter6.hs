7ad733f48a1d4a6d73e949977f57b2c2e23bd02c
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-11-06 17:46:55 +0200

    Initial commit

---
+++ Chapter6.hs
@@ -0,0 +1,14 @@
+module Chapter6 where
+
+-- https://www.reddit.com/r/HaskellBook/comments/79a3su/ch_6_on_a_scale_of_1_to_stupid/
+chk :: Eq b => (a -> b) -> a -> b -> Bool
+chk f a b = f a == b
+-- chk f a b = (==) (f a) b
+-- chk f a = (==) (f a)
+-- chk f a = (==) . f $ a
+-- chk f = (==) . f
+-- chk f = (.) (==) f
+-- chk = (.) (==)
+
+arith :: Num b => (a -> b) -> Integer -> a -> b
+arith f _ a = f a
