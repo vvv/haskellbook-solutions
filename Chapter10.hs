cb9d2d33092d57543a097466293ff9e836f97048
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-11-28 11:48:52 +0200

    Chapter10.hs: New file
    Incomplete exercises for Chapter 10.

---
+++ Chapter10.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wall -Werror #-}
+module Chapter10 where
+
+showFolds :: (String, String)
+showFolds = let f x y = concat ["(", x, "+", y, ")"]
+                xs = map show ([1..5] :: [Int])
+            in (foldl f "0" xs, foldr f "0" xs)
+
+showFoldr :: String
+showFoldr = snd showFolds
+
+showFoldl :: String
+showFoldl = fst showFolds
