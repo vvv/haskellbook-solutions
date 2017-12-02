9de9cd696cf7f71b02e1a2bd38d6ef62ed0333db
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-12-02 12:04:39 +0200

    src/DaPhone.hs: New file

--- src/Chapter11.hs
+++ src/Chapter11.hs
@@ -38,22 +38,6 @@ foldTree f z (Node left x right) =
 {-
 test :: IO ()
 test = do
-    let t1 :: BinaryTree Int
-        t1 = Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
-        t1' = Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
-    assertEq "mapTree" t1' $ mapTree (+1) t1
-
-    let t2 :: BinaryTree Int
-        t2 = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
-    assertEq "preorder" [2, 1, 3] $ preorder t2
-    assertEq "inorder" [1, 2, 3] $ inorder t2
-    assertEq "postorder" [1, 3, 2] $ postorder t2
-
-    let t3 :: BinaryTree Int
-        t3 = let i = insert' in i 10 $ i 0 $ i 2 $ i 4 $ i 6 Leaf
-    assertEq "foldTree-sum" 22 $ foldTree (+) 0 t3
-    assertEq "foldTree-list" [0, 2, 4, 6, 10] $ foldTree (:) [] t3
-
     assertEq "Vigenere"
         [ ('H','a'), ('e','b'), ('l','c'), ('l','a'), ('o','b'), (',','c')
         , (' ',' '), ('w','a'), ('o','b'), ('r','c'), ('l','a'), ('d','b')
