cb42c18d7841bf8d1481fd04bc335279dde7c6a2
Valery V. Vorotyntsev <valery.vv@gmail.com>
2018-05-15 22:46:49 +0100

    Cipher.hs: Implement Caesar cipher

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -1,6 +1,17 @@
-module Cipher where
+{-# OPTIONS_GHC -Wall -Werror #-}
 
-import Data.Char (chr, isAscii, isPrint, ord)
+module Cipher
+  ( KeyWord(..)
+  , PlainText(..)
+  , alphabet
+  , caesar
+  , cipherVigenere
+  , decode
+  , encode
+  , uncaesar
+  ) where
+
+import Data.Char (chr, isUpper, ord, toLower, toUpper)
 
 newtype PlainText = PlainText String
   deriving (Eq, Ord, Show)
@@ -8,8 +19,36 @@ newtype PlainText = PlainText String
 newtype KeyWord = KeyWord String
   deriving (Eq, Ord, Show)
 
+alphabet :: String
+alphabet = ['a'..'z']
+
+errorBadChar :: a
+errorBadChar = error "Unsupported character"
+
+mkCipher :: (Int -> Char -> Char) -> Int -> Char -> Char
+mkCipher f shift c | isUpper c = toUpper . f shift $ toLower c
+                   | otherwise = f shift c
+
+caesar :: Int -> Char -> Char
+caesar = mkCipher (caesar' False)
+
+uncaesar :: Int -> Char -> Char
+uncaesar = mkCipher (caesar' True)
+
+caesar' :: Bool -> Int -> Char -> Char
+caesar' _ _ c | c `notElem` alphabet = errorBadChar
+caesar' decode_p shift c =
+    let len = length alphabet
+        shift' = shift `mod` len
+        (c', c'') = if decode_p
+                    then (ord c - shift', c' + len)
+                    else (ord c + shift', c' - len)
+    in chr $ if c' < ord (head alphabet) || c' > ord (last alphabet)
+             then c''
+             else c'
+
 cipherVigenere :: KeyWord -> PlainText
-                -> String -- ^ Ciphertext.
+               -> String -- ^ Ciphertext.
 cipherVigenere (KeyWord "") _ = error "Empty keyword"
 cipherVigenere (KeyWord key) _
   | elem ' ' key = error "Keyword must not contain spaces"
@@ -21,16 +60,13 @@ cipherVigenere key plain = go key plain
     go (KeyWord (k:ks)) (PlainText (p:ps)) =
         encode p k : go (KeyWord ks) (PlainText ps)
 
-alphabet :: String
-alphabet = filter (\c -> isAscii c && isPrint c) $ map chr [0..127]
-
 encode :: Char -> Char -> Char
 encode p k | elem p alphabet && elem k alphabet =
     let base = ord (head alphabet)
         idx c = ord c - base
         delta = (idx p + idx k) `mod` (length alphabet)
     in chr (base + delta)
-encode _ _ = error "Only printable ASCII characters are supported"
+encode _ _ = errorBadChar
 
 -- XXX REFACTORME
 decode :: Char -> Char -> Char
@@ -39,4 +75,4 @@ decode p k | elem p alphabet && elem k alphabet =
         idx c = ord c - base
         delta = (idx p - idx k) `mod` (length alphabet)
     in chr (base + delta)
-decode _ _ = error "Only printable ASCII characters are supported"
+decode _ _ = errorBadChar
