3841dc4a209306fda334036f974bf57e7b007a66
Valery V. Vorotyntsev <valery.vv@gmail.com>
2018-06-19 23:12:07 +0300

    src/Cipher.hs: Complete rewrite
    + Handle errors properly.

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -1,78 +1,49 @@
-{-# OPTIONS_GHC -Wall -Werror #-}
-
 module Cipher
-  ( KeyWord(..)
-  , PlainText(..)
-  , alphabet
+  ( SomeText(CipherText,PlainText)
   , caesar
-  , cipherVigenere
-  , decode
-  , encode
-  , uncaesar
+  , vigenere
   ) where
 
-import Data.Char (chr, isUpper, ord, toLower, toUpper)
+-- XXX TODO: Support upper-case letters.
 
-newtype PlainText = PlainText String
-  deriving (Eq, Ord, Show)
+import Data.Char (ord)
+import Data.Either (partitionEithers)
 
-newtype KeyWord = KeyWord String
-  deriving (Eq, Ord, Show)
+data SomeText
+  = PlainText String
+  | CipherText String
+  deriving (Eq, Show)
 
 alphabet :: String
 alphabet = ['a'..'z']
 
-errorBadChar :: a
-errorBadChar = error "Unsupported character"
-
-mkCipher :: (Int -> Char -> Char) -> Int -> Char -> Char
-mkCipher f shift c | isUpper c = toUpper . f shift $ toLower c
-                   | otherwise = f shift c
-
-caesar :: Int -> Char -> Char
-caesar = mkCipher (caesar' False)
-
-uncaesar :: Int -> Char -> Char
-uncaesar = mkCipher (caesar' True)
-
-caesar' :: Bool -> Int -> Char -> Char
-caesar' _ _ c | c `notElem` alphabet = errorBadChar
-caesar' decode_p shift c =
-    let len = length alphabet
-        shift' = shift `mod` len
-        (c', c'') = if decode_p
-                    then (ord c - shift', c' + len)
-                    else (ord c + shift', c' - len)
-    in chr $ if c' < ord (head alphabet) || c' > ord (last alphabet)
-             then c''
-             else c'
-
-cipherVigenere :: KeyWord -> PlainText
-               -> String -- ^ Ciphertext.
-cipherVigenere (KeyWord "") _ = error "Empty keyword"
-cipherVigenere (KeyWord key) _
-  | elem ' ' key = error "Keyword must not contain spaces"
-cipherVigenere key plain = go key plain
-  where
-    go _ (PlainText "") = []
-    go (KeyWord "") ps = go key ps
-    go ks (PlainText (' ':ps)) = ' ' : go ks (PlainText ps)
-    go (KeyWord (k:ks)) (PlainText (p:ps)) =
-        encode p k : go (KeyWord ks) (PlainText ps)
+idx :: Char -> Int
+idx c = ord c - ord (head alphabet)
 
 encode :: Char -> Char -> Char
-encode p k | elem p alphabet && elem k alphabet =
-    let base = ord (head alphabet)
-        idx c = ord c - base
-        delta = (idx p + idx k) `mod` (length alphabet)
-    in chr (base + delta)
-encode _ _ = errorBadChar
+encode k a = cycle alphabet !! (idx a + idx k)
 
--- XXX REFACTORME
 decode :: Char -> Char -> Char
-decode p k | elem p alphabet && elem k alphabet =
-    let base = ord (head alphabet)
-        idx c = ord c - base
-        delta = (idx p - idx k) `mod` (length alphabet)
-    in chr (base + delta)
-decode _ _ = errorBadChar
+decode k b = cycle alphabet !! (idx b + length alphabet - idx k)
+
+tryTo :: (Char -> Char -> Char) -> (Char, Char) -> Either String Char
+tryTo f (k, a)
+  | k `notElem` alphabet = mkErr "keyword" k
+  | a `notElem` alphabet = mkErr "plaintext" a
+  | otherwise = Right (f k a)
+  where
+    mkErr src c = Left $ "Unsupported character in " ++ src ++ ": " ++ show c
+
+vigenere :: String -> SomeText -> Either String SomeText
+vigenere "" _ = Left "Empty keyword"
+vigenere keyword text =
+    let (xcode, txt, wrap) = case text of
+            PlainText s -> (encode, s, CipherText)
+            CipherText s -> (decode, s, PlainText)
+    in case partitionEithers $ map (tryTo xcode) $ zip (cycle keyword) txt of
+        ([], txt')   -> Right (wrap txt')
+        ((err:_), _) -> Left err
+
+caesar :: SomeText -> Either String SomeText
+caesar = let k = alphabet !! (length alphabet `div` 2)
+         in vigenere [k]
